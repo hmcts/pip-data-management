d958589e0d604d0f864b74c9f16e99305527ef5e
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2021-07-20 10:56:58 +0100

    Add/remove line breaks

--- src/main/java/uk/gov/hmcts/reform/demo/errorhandling/ExceptionResponse.java
+++ src/main/java/uk/gov/hmcts/reform/demo/errorhandling/ExceptionResponse.java
@@ -1,15 +1,19 @@
 package uk.gov.hmcts.reform.demo.errorhandling;
+
 import lombok.Data;
+
 import java.time.LocalDateTime;
 
 @Data
 public class ExceptionResponse {
 
-    /*The error message to return*/
-
+    /*
+    The error message to return
+    */
     private  String message;
 
-    /*The timestamp when error occurs*/
-    
+    /*
+    The timestamp when error occurs
+    */
     private LocalDateTime timestamp;
 }
