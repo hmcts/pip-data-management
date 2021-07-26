7ea0e9d2cc5f1b6c34dca8ba8cae5834f13ec61e
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-07-26 13:19:01 +0100

    PUB-529 - Updated tests for sonarqube

--- src/main/java/uk/gov/hmcts/reform/demo/errorhandling/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/demo/errorhandling/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
@@ -15,12 +14,11 @@ public class GlobalExceptionHandler {
      * Template exception handler, that handles a custom DataStorageNotFoundException,
      * and returns a 404 in the standard format.
      * @param ex The exception that has been thrown.
-     * @param request The request made to the endpoint.
      * @return The error response, modelled using the ExceptionResponse object.
      */
 
     @ExceptionHandler(DataStorageNotFoundException.class)
-    ResponseEntity<ExceptionResponse> handleDataStorageNotFound(DataStorageNotFoundException ex, WebRequest request) {
+    public ResponseEntity<ExceptionResponse> handleDataStorageNotFound(DataStorageNotFoundException ex) {
 
         ExceptionResponse exceptionResponse = new ExceptionResponse();
         exceptionResponse.setMessage(ex.getMessage());
