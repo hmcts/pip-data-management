c1929afa3603c2845be3bfb40aa206713601cb70
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2021-07-26 16:30:28 +0100

    PUB-529 - Jenkins Pipeline Code Coverage (#6)
    * PUB-529 - Fixed App and Dependency Check
    
    * PUB-529 - Updated tests for sonarqube

---
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/errorhandling/GlobalExceptionHandlerTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.rsecheck.errorhandling;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.demo.errorhandling.DataStorageNotFoundException;
+import uk.gov.hmcts.reform.demo.errorhandling.ExceptionResponse;
+import uk.gov.hmcts.reform.demo.errorhandling.GlobalExceptionHandler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class GlobalExceptionHandlerTest {
+
+    @Test
+    @DisplayName("Test that the response entity returned from the exception handler, "
+        + "contains the expected status code and body")
+    public void testHandleSubscriptionNotFoundMethod() {
+
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        DataStorageNotFoundException dataStorageNotFoundException
+            = new DataStorageNotFoundException("This is a test message");
+
+        ResponseEntity<ExceptionResponse> responseEntity =
+            globalExceptionHandler.handleDataStorageNotFound(dataStorageNotFoundException);
+
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), "Status code should be not found");
+        assertNotNull(responseEntity.getBody(), "Response should contain a body");
+        assertEquals("This is a test message", responseEntity.getBody().getMessage(),
+                     "The message should match the message passed in");
+    }
+}
