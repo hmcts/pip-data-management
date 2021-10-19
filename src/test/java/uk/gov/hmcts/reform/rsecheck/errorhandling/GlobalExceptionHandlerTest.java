151aa8145308913e3f891c1240cb383e9ce62e80
chris-pateman <86669094+chris-pateman@users.noreply.github.com>
2021-10-19 11:22:53 +0100

    PUB-807 Upgrade Template (#16)
    * PUB-760 chart setup
    
    * update ingress
    
    * updates
    
    * PUB-807 upgrade template
    
    * Merge branch 'PUB-760' into PUB-807
    
    * add back in guava
    
    * add back in guava
    
    * correct references
    
    * PUB-807 - Updated swagger
    
    Co-authored-by: ChrisS1512 <Christopher.Stacey@justice.gov.uk>

---
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/errorhandling/GlobalExceptionHandlerTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.rsecheck.errorhandling;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.pip.data.management.errorhandling.ExceptionResponse;
+import uk.gov.hmcts.reform.pip.data.management.errorhandling.GlobalExceptionHandler;
+import uk.gov.hmcts.reform.pip.data.management.errorhandling.exceptions.DataStorageNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class GlobalExceptionHandlerTest {
+
+    @Test
+    @DisplayName("Test that the response entity returned from the exception handler, "
+        + "contains the expected status code and body")
+    void testHandleSubscriptionNotFoundMethod() {
+
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        DataStorageNotFoundException dataStorageNotFoundException
+            = new DataStorageNotFoundException("This is a test message");
+
+        ResponseEntity<ExceptionResponse> responseEntity =
+            globalExceptionHandler.handle(dataStorageNotFoundException);
+
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode(), "Status code should be not found");
+        assertNotNull(responseEntity.getBody(), "Response should contain a body");
+        assertEquals("This is a test message", responseEntity.getBody().getMessage(),
+                     "The message should match the message passed in");
+    }
+}
