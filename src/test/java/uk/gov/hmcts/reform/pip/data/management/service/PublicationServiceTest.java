6356ceea537eb0a8315734faf6bff9a6b5587ad6
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-10-27 10:58:25 +0100

    Added service test

---
+++ src/test/java/uk/gov/hmcts/reform/pip/data/management/service/PublicationServiceTest.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.pip.data.management.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.pip.data.management.database.AzureTableService;
+import uk.gov.hmcts.reform.pip.data.management.errorhandling.exceptions.DuplicatePublicationException;
+import uk.gov.hmcts.reform.pip.data.management.models.publication.Artefact;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PublicationServiceTest {
+
+    @Mock
+    AzureTableService azureTableService;
+
+    @InjectMocks
+    PublicationService publicationService;
+
+    private static final String ARTEFACT_ID = "1234";
+    private static final String ROW_ID = "1234-1234";
+
+    @Test
+    void testCreationOfNewArtefact() {
+
+        Artefact artefact = Artefact.builder()
+            .artefactId(ARTEFACT_ID)
+            .build();
+
+        when(azureTableService.getPublication(ARTEFACT_ID)).thenReturn(Optional.empty());
+        when(azureTableService.createPublication(artefact)).thenReturn(ROW_ID);
+
+        String returnedUuid = publicationService.createPublication(artefact);
+
+        assertEquals(ROW_ID, returnedUuid);
+    }
+
+    @Test
+    void testCreationOfExistingArtefact() {
+
+        Artefact artefact = Artefact.builder()
+            .artefactId(ARTEFACT_ID)
+            .build();
+
+        when(azureTableService.getPublication(ARTEFACT_ID)).thenReturn(Optional.of(artefact));
+
+        DuplicatePublicationException duplicatePublicationException =
+            assertThrows(DuplicatePublicationException.class, () -> publicationService.createPublication(artefact));
+
+        assertEquals(String.format("Duplicate publication found with ID %s", ARTEFACT_ID),
+                     duplicatePublicationException.getMessage());
+    }
+
+}
