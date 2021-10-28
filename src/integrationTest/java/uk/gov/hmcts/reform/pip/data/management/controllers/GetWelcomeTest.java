7f0976c67b3f58cad087e5900f0ab916bad3fa91
Kurt Plaschke <k.plaschke@hull.ac.uk>
2021-10-28 12:36:02 +0100

    fixed intergration test

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/data/management/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/data/management/controllers/GetWelcomeTest.java
@@ -16,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@SuppressWarnings("PMD.UnusedPrivateField")
 class GetWelcomeTest {
 
     @Autowired
