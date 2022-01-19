bb3902c33b19ee1fc87819f62f7eab2dd1130b03
Daniel Furnivall <danielfurnivall@gmail.com>
2022-01-19 16:21:55 +0000

    fixed serialVersionUID duplication issue

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/errorhandling/exceptions/EmptyRequiredHeaderException.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/errorhandling/exceptions/EmptyRequiredHeaderException.java
@@ -4,7 +4,7 @@ package uk.gov.hmcts.reform.pip.data.management.errorhandling.exceptions;
  * Custom exception which handles all header validation issues.
  */
 public class EmptyRequiredHeaderException extends HeaderValidationException {
-    private static final long serialVersionUID = 3910456611414432879L;
+    private static final long serialVersionUID = 2810432611414432879L;
 
     /**
      * Constructor for the HeaderValidationException.
