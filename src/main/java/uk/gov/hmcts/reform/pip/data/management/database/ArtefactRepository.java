8704db44f3e717c29b772bf79e6ec6a6a09b89d7
Kurt <kurtplaschke@hotmail.co.uk>
2022-02-22 16:26:47 +0000

    PUB-948 - Added search API for artefacts under case values (#57)
    * Added search API for artefacts under case values
    
    * added case name search functionality
    
    * updated extractor to pull a case object from blob rather than individual attr. so that links can be made to case name, number and urn
    
    * Further changes to tests
    
    * Fixed checkstyles, PMD and updated tests
    
    * PUB-948 - Updated to cases
    
    * Fixed error with repository
    
    * PUB-948 - Added two tests
    
    * resolved merge conflicts
    
    Co-authored-by: ChrisS1512 <Christopher.Stacey@justice.gov.uk>
    Co-authored-by: Alberto Hortiguela <80695759+alberto-hortiguela@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/database/ArtefactRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/database/ArtefactRepository.java
@@ -13,9 +13,17 @@ import java.util.Optional;
 @Repository
 public interface ArtefactRepository extends JpaRepository<Artefact, Long> {
 
+    String INITIAL_SELECT =
+         "SELECT DISTINCT on (artefact.artefact_id) artefact.* FROM ARTEFACT INNER JOIN (SELECT "
+         + "artefact_id, json_array_elements(search -> 'cases') caseDetails FROM artefact) searchDetails ON artefact"
+         + ".artefact_id = searchDetails.artefact_id ";
+
+    String SEARCH_TERM_PARAM = "searchTerm";
     String ARTEFACT_ID_PARAM = "artefact_id";
     String CURRENT_DATE_PARAM = "curr_date";
-    String SEARCH_VAL_PARAM = "searchVal";
+    String SEARCH_VAL_PARAM = "searchValue";
+    String COURT_ID_PARAM = "courtId";
+    String CASE_NAME_PARAM = "caseName";
 
     Optional<Artefact> findBySourceArtefactIdAndProvenance(String sourceArtefactId, String provenance);
 
@@ -31,15 +39,43 @@ public interface ArtefactRepository extends JpaRepository<Artefact, Long> {
     Optional<Artefact> findByArtefactIdUnverified(@Param(ARTEFACT_ID_PARAM) String artefactId,
                                                   @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
 
-    @Query(value = "select * from Artefact where court_id = :searchVal and display_from < "
+    @Query(value = "select * from Artefact where court_id = :courtId and display_from < "
         + ":curr_date and (display_to> :curr_date or display_to is null)",
         nativeQuery = true)
-    List<Artefact> findArtefactsBySearchVerified(@Param(SEARCH_VAL_PARAM) String searchVal,
-                                                 @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
+    List<Artefact> findArtefactsByCourtIdVerified(@Param(COURT_ID_PARAM) String courtId,
+                                                  @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
 
-    @Query(value = "select * from Artefact where court_id = :searchVal and sensitivity = 'PUBLIC' "
+    @Query(value = "select * from Artefact where court_id = :courtId and sensitivity = 'PUBLIC' "
         + "and display_from < :curr_date and (display_to> :curr_date or display_to is null)",
         nativeQuery = true)
-    List<Artefact> findArtefactsBySearchUnverified(@Param(SEARCH_VAL_PARAM) String searchVal,
-                                                   @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
+    List<Artefact> findArtefactsByCourtIdUnverified(@Param(COURT_ID_PARAM) String courtId,
+                                                    @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
+
+    @Query(value = INITIAL_SELECT + "WHERE LOWER(searchDetails.caseDetails ->> 'caseName') LIKE LOWER"
+        + "('%' || :caseName || '%') and display_from < :curr_date and (display_to > :curr_date or display_to is null)",
+        nativeQuery = true)
+    List<Artefact> findArtefactByCaseNameVerified(@Param(CASE_NAME_PARAM) String caseName,
+                                                  @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
+
+    @Query(value = INITIAL_SELECT + "WHERE LOWER(searchDetails.caseDetails ->> 'caseName') LIKE "
+        + "LOWER('%' || :caseName || '%') and display_from < :curr_date and (display_to > :curr_date or display_to "
+        + "is null) and sensitivity = 'PUBLIC'", nativeQuery = true)
+    List<Artefact> findArtefactByCaseNameUnverified(@Param(CASE_NAME_PARAM) String caseName,
+                                                    @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
+
+
+    @Query(value = INITIAL_SELECT + "WHERE searchDetails.caseDetails ->> :searchTerm = :searchValue and "
+        + "display_from < :curr_date and (display_to > :curr_date or display_to is null)", nativeQuery = true)
+    List<Artefact> findArtefactBySearchVerified(@Param(SEARCH_TERM_PARAM) String searchTerm,
+                                                @Param(SEARCH_VAL_PARAM) String searchVal,
+                                                @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
+
+    @Query(value = INITIAL_SELECT + "WHERE searchDetails.caseDetails ->> :searchTerm = :searchValue "
+        + "and display_from < :curr_date and (display_to > :curr_date or display_to is null) and sensitivity = "
+        + "'PUBLIC'",
+        nativeQuery = true)
+    List<Artefact> findArtefactBySearchUnverified(@Param(SEARCH_TERM_PARAM) String searchTerm,
+                                                  @Param(SEARCH_VAL_PARAM) String searchVal,
+                                                  @Param(CURRENT_DATE_PARAM) LocalDateTime currentDate);
+
 }
