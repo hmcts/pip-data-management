999c5c9ed1aeadab8fa653b249e3859367f5b055
Daniel Furnivall <danielfurnivall@gmail.com>
2022-03-08 15:59:13 +0000

    checkstyle

--- src/test/java/uk/gov/hmcts/reform/pip/data/management/service/SubscriptionManagementServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/data/management/service/SubscriptionManagementServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.pip.data.management.Application;
