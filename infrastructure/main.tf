28358bdde2fc67fc311c583a33ae41b16cd7fb0d
Christopher Pateman <christopher.pateman@version1.com>
2022-03-30 11:26:26 +0100

    Update with TF

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,11 @@
 provider "azurerm" {
   features {}
 }
+
+data "azurerm_resource_group" "example" {
+  name = "pip-ss-sbox-rg"
+}
+
+output "id" {
+  value = data.azurerm_resource_group.example.id
+} 
