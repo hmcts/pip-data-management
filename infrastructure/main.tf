32a615874d1afed8ed555f6abe9f983af4f077be
Christopher Pateman <christopher.pateman@version1.com>
2022-03-30 11:41:39 +0100

    Remove unneeded Tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,10 +2,3 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "example" {
-  name = "pip-ss-sbox-rg"
-}
-
-output "id" {
-  value = data.azurerm_resource_group.example.id
-} 
