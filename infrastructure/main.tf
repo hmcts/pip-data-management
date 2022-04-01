f25a0b495e911146101e86ee69cfe5ada7e1777b
Christopher Pateman <christopher.pateman@version1.com>
2022-04-01 08:40:32 +0100

    Create DB and update charts

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,20 @@
-provider "azurerm" {
-  features {}
+locals {
+  prefix              = "${var.product}-ss"
+  prefix_no_special   = replace(local.prefix, "-", "")
+  resource_group_name = "${local.prefix}-${var.env}-rg"
+  key_vault_name      = "${local.prefix}-kv-${var.env}"
+  env_long_name       = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
+}
+data "azurerm_client_config" "current" {}
+
+
+data "azurerm_subnet" "iaas" {
+  name                 = "iaas"
+  resource_group_name  = "ss-${var.env}-network-rg"
+  virtual_network_name = "ss-${var.env}-vnet"
+}
+
+data "azurerm_key_vault" "kv" {
+  name                = local.key_vault_name
+  resource_group_name = local.resource_group_name
 }
