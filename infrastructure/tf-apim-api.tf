696f823b9f6d4e44f272c5beafcf2872b0e42584
Christopher Pateman <christopher.pateman@version1.com>
2022-04-08 10:36:52 +0100

    Update sub bool, add get, update backend service URL

--- infrastructure/tf-apim-api.tf
+++ infrastructure/tf-apim-api.tf
@@ -9,17 +9,18 @@ module "apim_api" {
   count  = local.deploy_apim
   source = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
 
-  api_mgmt_name  = local.apim_name
-  api_mgmt_rg    = local.apim_rg
-  display_name   = local.api_name
-  name           = local.api_name
-  path           = "${var.product}/data-management"
-  product_id     = data.azurerm_api_management_product.apim_product[0].product_id
-  protocols      = ["https"]
-  revision       = "1"
-  service_url    = "https://pip-data-management.${local.env_long_name}.platform.hmcts.net"
-  swagger_url    = file("./resources/swagger/api-swagger.json")
-  content_format = "openapi+json"
+  api_mgmt_name         = local.apim_name
+  api_mgmt_rg           = local.apim_rg
+  display_name          = local.api_name
+  name                  = local.api_name
+  path                  = "${var.product}/data-management"
+  product_id            = data.azurerm_api_management_product.apim_product[0].product_id
+  protocols             = ["https"]
+  revision              = "1"
+  service_url           = "https://pip-data-management.${local.env_long_name}.platform.hmcts.net"
+  swagger_url           = file("./resources/swagger/api-swagger.json")
+  content_format        = "openapi+json"
+  subscription_required = false
 }
 
 module "apim_api_policy" {
