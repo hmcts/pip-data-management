5a5b37548f1b6629a3d046a59598ddd3eec81f63
Christopher Pateman <christopher.pateman@version1.com>
2022-04-27 09:09:15 +0100

    Update logger  ID

--- infrastructure/tf-apim-logger.tf
+++ infrastructure/tf-apim-logger.tf
@@ -5,7 +5,7 @@ resource "azurerm_api_management_api_diagnostic" "api_logs" {
   resource_group_name      = local.apim_rg
   api_management_name      = local.apim_name
   api_name                 = local.apim_api_name
-  api_management_logger_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.apim_rg}/providers/Microsoft.ApiManagement/service/${local.apim_name}loggers/sds-api-mgmt-${local.env}-logger"
+  api_management_logger_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.apim_rg}/providers/Microsoft.ApiManagement/service/${local.apim_name}/loggers/sds-api-mgmt-${local.env}-logger"
 
   sampling_percentage       = 5.0
   always_log_errors         = true
