90f1a17c627a4733a4b96bcfc582b8b40cf6e23e
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-05-06 12:05:07 +0100

    Moved to second version

--- src/main/resources/db/migration/V1.2__Sjp_court_load.sql
+++ src/main/resources/db/migration/V1.2__Sjp_court_load.sql
@@ -1,12 +1,1 @@
-CREATE TABLE IF NOT EXISTS court (
-  court_id integer NOT NULL PRIMARY KEY,
-  jurisdiction text[],
-  name character varying(255),
-  region character varying(255)
-);
-
-INSERT INTO court(court_id, jurisdiction, name)
-VALUES(0, '{}', 'Single Justice Procedure (SJP)')
-ON CONFLICT (court_id) DO NOTHING;
-
 UPDATE court SET name = 'Single Justice Procedure' WHERE court_id = 0;
