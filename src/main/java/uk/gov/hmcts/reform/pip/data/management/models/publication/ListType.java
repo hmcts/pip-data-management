f9e8a665e4ff6c5deef05d8c76a0f021de0a6fce
KianKwa <80688761+KianKwa@users.noreply.github.com>
2022-09-20 14:11:46 +0100

    PUB-1347 Add IAC_DAILY_LIST validation schema (#158)
    * Add IAC_DAILY_LIST validation schema
    
    * PMD fixes
    
    * Add interpreter language to master schema and IAC daily list schema

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/models/publication/ListType.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/models/publication/ListType.java
@@ -20,7 +20,8 @@ public enum ListType {
     FAMILY_DAILY_CAUSE_LIST(LocationType.VENUE),
     CIVIL_AND_FAMILY_DAILY_CAUSE_LIST(LocationType.VENUE),
     COP_DAILY_CAUSE_LIST(LocationType.VENUE),
-    SSCS_DAILY_LIST(LocationType.REGION);
+    SSCS_DAILY_LIST(LocationType.REGION),
+    IAC_DAILY_LIST(LocationType.VENUE);
 
     /**
      * Flag that represents the Location Type level the list displays at.
