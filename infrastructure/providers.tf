be496a96386c9abe94eae69b7d4cae768d9082ce
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-11-08 17:30:00 +0000

    Updated provider

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.0.4"
   required_providers {
     azurerm = "3.14"
+    postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = ">=1.17.1"
+    }
   }
 }
 
