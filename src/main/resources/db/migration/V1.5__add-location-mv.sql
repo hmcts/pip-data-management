57f3b85416659e062ee39b4ffb046f353248d666
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-11-21 17:00:16 +0000

    Added location

--- src/main/resources/db/migration/V1.5__add-location-mv.sql
+++ src/main/resources/db/migration/V1.5__add-location-mv.sql
@@ -1,6 +1,17 @@
 --
 -- Creates the materialised view for location if it doesn't exist.
 --
+CREATE TABLE IF NOT EXISTS location (
+    location_id integer NOT NULL PRIMARY KEY,
+    name varchar(255),
+    region text[],
+    location_type varchar(255),
+    jurisdiction text[],
+    welsh_name varchar(255),
+    welsh_jurisdiction text[],
+    welsh_region varchar(255)
+);
+
 CREATE MATERIALIZED VIEW IF NOT EXISTS sdp_mat_view_location AS
 SELECT location.location_id,
        location.name,
