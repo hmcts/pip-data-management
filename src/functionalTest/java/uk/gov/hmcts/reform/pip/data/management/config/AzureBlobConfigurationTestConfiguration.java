c5f4c64d15934a416aa72dd018f2863a2e1d518d
Kian Kwa <kian.kwa@justice.gov.uk>
2023-08-24 18:05:31 +0100

    Fix checkstyle complaint

--- src/functionalTest/java/uk/gov/hmcts/reform/pip/data/management/config/AzureBlobConfigurationTestConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pip/data/management/config/AzureBlobConfigurationTestConfiguration.java
@@ -6,7 +6,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 /**
