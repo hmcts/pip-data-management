0853eddb133332c00dc4c3af206ad1e3da5525c6
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2023-09-04 09:13:33 +0100

    PUB-1984 - Removed unused import

--- src/test/java/uk/gov/hmcts/reform/pip/data/management/service/schemavalidation/SjpPublicListTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/data/management/service/schemavalidation/SjpPublicListTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.jayway.jsonpath.internal.filter.ValueNode;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
