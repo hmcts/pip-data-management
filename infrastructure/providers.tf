84c625caf4665a92349948c614be2c7f0f9cdbd1
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-09-28 15:37:42 +0100

    Added custom db host name

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -5,7 +5,7 @@ terraform {
       version = "3.71.0"
     }
     postgresql = {
-      source = "cyrilgdn/postgresql"
+      source  = "cyrilgdn/postgresql"
       version = ">=1.17.1"
     }
   }
--- infrastructure/tf-db-flexible.tf
+++ infrastructure/tf-db-flexible.tf
@@ -1,21 +1,26 @@
+locals {
+  db_host_name = "flexible-${var.product}-${var.component}"
+}
+
 module "postgresql" {
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
 
-  source    = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
-  product   = var.product
-  component = var.component
-  location  = var.location
-  env       = var.env
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  name                 = local.db_host_name
+  product              = var.product
+  component            = var.component
+  location             = var.location
+  env                  = var.env
   pgsql_admin_username = local.postgresql_user
   pgsql_databases = [
     {
       name : local.db_name
     }
   ]
-  common_tags        = var.common_tags
-  business_area      = "sds"
+  common_tags   = var.common_tags
+  business_area = "sds"
   pgsql_version = "15"
 
   admin_user_object_id = var.jenkins_AAD_objectId
