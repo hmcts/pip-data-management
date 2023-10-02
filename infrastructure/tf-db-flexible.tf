7f35963a5a3362eeebc4a3208f78ca00033e0959
Josh <joshblackmoor@users.noreply.github.com>
2023-10-02 09:31:13 +0100

    PUB-2130 Create Postgres 15 Azure Flexible Server (#463)
    * WIP
    
    * Added new flexible DB
    
    * Updated local names
    
    * Revert change for current DB
    
    * Updated flexible secret
    
    * Update local vars
    
    * Added provider for postgres
    
    * Added provider to module
    
    * Updated db name
    
    * made sds lower case for flexible db
    
    * add jenkins admin id
    
    * Added custom db host name

---
+++ infrastructure/tf-db-flexible.tf
@@ -0,0 +1,29 @@
+locals {
+  db_host_name = "flexible-${var.product}-${var.component}"
+}
+
+module "postgresql" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  name                 = local.db_host_name
+  product              = var.product
+  component            = var.component
+  location             = var.location
+  env                  = var.env
+  pgsql_admin_username = local.postgresql_user
+  pgsql_databases = [
+    {
+      name : local.db_name
+    }
+  ]
+  common_tags   = var.common_tags
+  business_area = "sds"
+  pgsql_version = "15"
+
+  admin_user_object_id = var.jenkins_AAD_objectId
+}
+
+# SDP access and MV required in here. Will be done at migration
