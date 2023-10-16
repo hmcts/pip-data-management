4c3f6bd513cb5ebb950e2ca21e6f775d786ea0e9
Natasha <natashaalker@yahoo.co.uk>
2023-10-16 12:13:45 +0100

    Pub 2164 (#471)
    * add superseded_count to the publications tab of MI report
    
    * update test
    
    * update string builder size to fix pmd issue
    
    * add suppression
    
    * update suppressions
    
    * update suppressions for requested change
    
    * update suppressions for requested change
    
    * update suppressions
    
    * update suppressions
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/database/ArtefactRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/database/ArtefactRepository.java
@@ -114,7 +114,7 @@ public interface ArtefactRepository extends JpaRepository<Artefact, Long> {
     Integer countNoMatchArtefacts();
 
     @Query(value = "SELECT cast(artefact_id as text), display_from, display_to, language, "
-        + "provenance, sensitivity, source_artefact_id, type, content_date, location_id, list_type "
+        + "provenance, sensitivity, source_artefact_id, superseded_count, type, content_date, location_id, list_type "
         + "FROM artefact",
         nativeQuery = true)
     List<String> getMiData();
