dcda9614eec6846dfcddaeb1e6de1aab05ec69b3
Kian Kwa <kian.kwa@justice.gov.uk>
2023-10-17 10:48:40 +0100

    OPA_PRESS_LIST validation schema

--- src/test/java/uk/gov/hmcts/reform/pip/data/management/service/ValidationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/data/management/service/ValidationServiceTest.java
@@ -312,7 +312,9 @@ class ValidationServiceTest {
             Arguments.of(ListType.CARE_STANDARDS_LIST,
                          "mocks/care-standards-list/careStandardsList.json"),
             Arguments.of(ListType.PRIMARY_HEALTH_LIST,
-                         "mocks/primary-health-list/primaryHealthList.json")
+                         "mocks/primary-health-list/primaryHealthList.json"),
+            Arguments.of(ListType.OPA_PRESS_LIST,
+                         "mocks/opa-press-list/opaPressList.json")
         );
     }
 }
