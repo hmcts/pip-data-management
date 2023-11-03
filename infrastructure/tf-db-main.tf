4898e9e55541f3f96ef49ba3ee0ec7d0636870b2
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2023-11-03 09:03:30 +0000

    PUB-2222 -  Remove old postgres (#494)
    * PUB-2222 - Removed old config
    
    * PUB-2222 - Updated module dependencies
    
    * PUB-2222 - Updated count
    
    * PUB-2222 - Removed old postgres
    
    * PUB-2222 - Removed old postgres
    
    * PUB-2222 - Removed old DB

--- infrastructure/tf-db-main.tf
+++ infrastructure/tf-db-main.tf
@@ -22,4 +22,10 @@ module "database" {
 
   sku_name = var.env == "stg" || var.env == "prod" || var.env == "test" ? "GP_Gen5_8" : "GP_Gen5_2"
 
+  count    = var.env == "sbox" || var.env == "test" || var.env == "demo" ? 0 : 1
+}
+
+moved {
+  from = module.database
+  to   = module.database[0]
 }
