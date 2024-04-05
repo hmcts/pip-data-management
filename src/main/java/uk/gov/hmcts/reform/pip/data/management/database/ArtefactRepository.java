61e4eae4b75546563c41a0c34f873f6decb49d96
Kian Kwa <kian.kwa@justice.gov.uk>
2024-04-05 15:09:35 +0100

    Checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/database/ArtefactRepository.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/database/ArtefactRepository.java
@@ -12,7 +12,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface ArtefactRepository extends JpaRepository<Artefact, Long> {
