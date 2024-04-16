0bdfcc1b9c228a6a0b12cc5095a9113cdd8561c8
Kian Kwa <kian.kwa@justice.gov.uk>
2024-04-16 15:52:21 +0100

    Fixed checkstyle issue

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/models/location/Location.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/models/location/Location.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
