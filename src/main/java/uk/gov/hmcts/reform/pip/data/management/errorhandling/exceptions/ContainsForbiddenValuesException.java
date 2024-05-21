647531bd384265868f90655c090685fc565823f2
Natasha Alker <NatashaAlker@yahoo.co.uk>
2024-05-21 14:15:41 +0100

    add generated id

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/errorhandling/exceptions/ContainsForbiddenValuesException.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/errorhandling/exceptions/ContainsForbiddenValuesException.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.pip.data.management.errorhandling.exceptions;
 
+import java.io.Serial;
+
 /**
  * This exception class handles fields that contain unwanted elements such as html tags.
  */
 public class ContainsForbiddenValuesException extends RuntimeException {
-
-    private static final long serialVersionUID = 4824563905241589732L;
+    @Serial
+    private static final long serialVersionUID = -8267692849518367195L;
 
     /**
      * Constructor for the Exception.
