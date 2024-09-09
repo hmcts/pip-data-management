a09354160916c06e9844090e2615ac82f9d351d4
Kian Kwa <kian.kwa@justice.gov.uk>
2024-09-09 16:54:06 +0100

    Added configuration annotation

--- src/functionalTest/java/uk/gov/hmcts/reform/pip/data/management/config/AzureBlobTestConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pip/data/management/config/AzureBlobTestConfiguration.java
@@ -7,8 +7,10 @@ import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.azure.storage.common.StorageSharedKeyCredential;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+@Configuration
 @Profile("functional")
 @SuppressWarnings("PMD.SystemPrintln")
 public class AzureBlobTestConfiguration {
