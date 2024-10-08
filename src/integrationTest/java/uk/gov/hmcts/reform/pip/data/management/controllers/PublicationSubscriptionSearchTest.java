27a13b438ba8f1f3916d5e138d86943f4c2571eb
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-10-08 09:05:12 +0100

    PUB-2612 - Updated subscriptions

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/data/management/controllers/PublicationSubscriptionSearchTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/data/management/controllers/PublicationSubscriptionSearchTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
