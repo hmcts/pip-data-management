7d4b172bddf2ef44a075cdd62dc65583b554aede
Kian Kwa <kian.kwa@justice.gov.uk>
2025-02-10 09:55:22 +0000

    Add empty spring boot test classes

--- src/smokeTest/java/uk/gov/hmcts/reform/pip/data/management/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/pip/data/management/SmokeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.pip.data.management.utils.SmokeTestBase;
 import uk.gov.hmcts.reform.pip.model.publication.ArtefactType;
@@ -34,6 +35,7 @@ import static uk.gov.hmcts.reform.pip.data.management.config.PublicationConfigur
 import static uk.gov.hmcts.reform.pip.data.management.config.PublicationConfiguration.SENSITIVITY_HEADER;
 import static uk.gov.hmcts.reform.pip.data.management.config.PublicationConfiguration.TYPE_HEADER;
 
+@SpringBootTest(classes = {})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("smoke")
 class SmokeTest extends SmokeTestBase {
