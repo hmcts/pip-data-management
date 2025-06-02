01e0f695c5a6c51f30b730dcda133d150e3ea850
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-06-02 14:10:14 +0100

    add missing autowire

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/controllers/LocationMetadataController.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/controllers/LocationMetadataController.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.pip.data.management.controllers;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -35,6 +36,7 @@ public class LocationMetadataController {
 
     private final LocationMetadataService locationMetaDataService;
 
+    @Autowired
     public LocationMetadataController(LocationMetadataService locationMetaDataService) {
         this.locationMetaDataService = locationMetaDataService;
     }
