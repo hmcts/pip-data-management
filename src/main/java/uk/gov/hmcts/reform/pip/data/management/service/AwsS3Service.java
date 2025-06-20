e450c057128d84d68f1403ac98a101c644a50208
kian.kwa <kian.kwa@justice.gov.uk>
2025-06-20 12:15:10 +0100

    Fixed checkstyle

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/service/AwsS3Service.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/service/AwsS3Service.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.pip.data.management.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
