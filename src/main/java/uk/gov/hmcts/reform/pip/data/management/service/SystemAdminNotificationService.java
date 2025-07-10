a6711c67b855fe4ca7b5d68d82d0669bb5e09414
kian.kwa <kian.kwa@justice.gov.uk>
2025-07-10 16:50:23 +0100

    Mark a service class correctly

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/service/SystemAdminNotificationService.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/service/SystemAdminNotificationService.java
@@ -2,12 +2,14 @@ package uk.gov.hmcts.reform.pip.data.management.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.pip.model.system.admin.ActionResult;
 import uk.gov.hmcts.reform.pip.model.system.admin.ChangeType;
 
 import java.util.List;
 import java.util.stream.Stream;
 
+@Service
 public class SystemAdminNotificationService {
     private final AccountManagementService accountManagementService;
     private final PublicationServicesService publicationServicesService;
