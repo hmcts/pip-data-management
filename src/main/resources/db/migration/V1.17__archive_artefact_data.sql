84429a50e978bff22665376a55b5482e50defcb5
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-08-11 16:20:33 +0100

    change database script

--- src/main/resources/db/migration/V1.17__archive_artefact_data.sql
+++ src/main/resources/db/migration/V1.17__archive_artefact_data.sql
@@ -4,72 +4,85 @@
 -- Check DB/Server Name
 DO
 $ServerCheck$
-	DECLARE
-correctServer varchar = 'flexible-pip-data-management-prod';
-		correctDBName varchar = 'datamanagement';
-		currentServer varchar;
-		currentDatabase varchar;
-BEGIN
+  DECLARE
+    correctServer varchar := 'flexible-pip-data-management-prod';
+    correctDBName varchar := 'datamanagement';
+    currentServer varchar;
+    currentDatabase varchar;
+  BEGIN
+    SELECT replace(setting, '-data', ''), current_database()
+    INTO currentServer, currentDatabase
+    FROM pg_settings
+    WHERE name = 'azure.customer_resource_group';
 
-select replace(setting,'-data',''), current_database()
-into currentServer,currentDatabase
-from pg_settings
-where name='azure.customer_resource_group';
+    IF currentServer <> correctServer THEN
+      RAISE EXCEPTION
+        USING DETAIL = 'Wrong SERVER! Server [' || currentServer || '] should be [' || correctServer || ']';
+    END IF;
 
-if currentServer <> correctServer THEN
-	    RAISE EXCEPTION
-	      USING DETAIL = 'Wrong SERVER! Server [' || currentServer|| '] should be [' || correctServer|| ']';
-END IF;
-        if currentDatabase <> correctDBName THEN
-	    RAISE EXCEPTION
-	      USING DETAIL = 'Wrong DB! DB [' || currentDatabase || '] should be [' || correctDBName|| ']';
-END IF;
-		RAISE NOTICE E'CORRECT Server and DB : Server [%], Database [%]\n',currentServer,currentDatabase;
-END
+    IF currentDatabase <> correctDBName THEN
+      RAISE EXCEPTION
+        USING DETAIL = 'Wrong DB! DB [' || currentDatabase || '] should be [' || correctDBName || ']';
+    END IF;
+
+    RAISE NOTICE 'CORRECT Server and DB: Server [%], Database [%]', currentServer, currentDatabase;
+  END
 $ServerCheck$;
 
---DTSRD-xxxxx - move archive artefacts to artefact_archived table
 DO
 $CR$
-DECLARE
-TicketNumber varchar(10) := 'DTSRD-xxxxx';
-  AffectedRows integer;
-  StatusText varchar(20);
-  QueryExpectedResult int := 6;
+  DECLARE
+    TicketNumber varchar(50) := 'DTSRD-xxxxx';
+    AffectedRows integer;
+    StatusText varchar(100);
+    QueryExpectedResult int := 13;
+    SecondQueryExpectedResult int := 13;
+  BEGIN
+    -- Step 1: Insert archived artefacts
+    StatusText := 'Step 1';
+    INSERT INTO public.artefact_archived (artefact_id, content_date, display_from, display_to, is_flat_file,
+                                          "language", list_type, location_id, provenance, sensitivity, type,
+                                          last_received_date, superseded_count, archived_date, is_manually_deleted)
+    SELECT artefact_id, content_date, display_from, display_to, is_flat_file,
+           "language", list_type, location_id, provenance, sensitivity, type,
+           last_received_date, superseded_count, LOCALTIMESTAMP, TRUE
+    FROM artefact
+    WHERE is_archived = TRUE;
 
-BEGIN
-    -- Step 1
-    StatusText='Step 1';
-INSERT INTO artefact_archived (artefact_id, content_date, display_from, display_to, is_flat_file,
-                               "language", list_type,location_id,  provenance, sensitivity, type,
-                               last_received_date, superseded_count, archived_date, is_manually_deleted)
-SELECT artefact_id, content_date, display_from, display_to, is_flat_file,
-       "language", list_type,location_id, provenance, sensitivity, type,
-       last_received_date, superseded_count, LOCALTIMESTAMP, TRUE
-FROM artefact
-WHERE is_archived = TRUE;
+    GET DIAGNOSTICS AffectedRows = ROW_COUNT;
+    IF AffectedRows <> QueryExpectedResult THEN
+      RAISE EXCEPTION '% % : ERROR wrong number of rows added --> %', TicketNumber, StatusText, AffectedRows;
+    END IF;
 
-GET DIAGNOSTICS AffectedRows = ROW_COUNT;
-if AffectedRows <> QueryExpectedResult then
-       RAISE EXCEPTION '% % : ERROR wrong number of rows added --> %',TicketNumber,StatusText,AffectedRows;
-end if;
-    RAISE NOTICE '% % : Rows INSERT by query=%', TicketNumber,StatusText, AffectedRows ;
+    RAISE NOTICE '% % : Rows INSERTED by query = %', TicketNumber, StatusText, AffectedRows;
 
-	RAISE NOTICE E'% SUCCESSFUL COMPLETION, all changes COMMITTED.\n', TicketNumber;
+    -- Step 2: Delete archived artefacts
+    StatusText := 'Step 2';
+    DELETE FROM artefact
+    WHERE is_archived = TRUE;
 
-EXCEPTION
-    WHEN others THEN
-    RAISE NOTICE '% % : ERROR RAISED, all changes ROLLED BACK', TicketNumber, StatusText;
-    RAISE EXCEPTION
-        USING DETAIL = 'ERROR:Change Failed [' || SQLSTATE|| ']' || SQLERRM;
-        --transaction is rolled back;
-END
+    GET DIAGNOSTICS AffectedRows = ROW_COUNT;
+    IF AffectedRows <> SecondQueryExpectedResult THEN
+      RAISE EXCEPTION '% % : ERROR wrong number of rows deleted --> %', TicketNumber, StatusText, AffectedRows;
+    END IF;
+
+    RAISE NOTICE '% % : Rows DELETED by query = %', TicketNumber, StatusText, AffectedRows;
+
+    RAISE NOTICE '% SUCCESSFUL COMPLETION, all changes COMMITTED.', TicketNumber;
+
+  EXCEPTION
+    WHEN OTHERS THEN
+      RAISE NOTICE '% % : ERROR RAISED, all changes ROLLED BACK', TicketNumber, StatusText;
+      RAISE EXCEPTION
+        USING DETAIL = 'ERROR: Change Failed [' || SQLSTATE || '] ' || SQLERRM;
+  END
 $CR$;
 
 /*
 Output will be:
 NOTICE:  CORRECT Server and DB : Server [flexible-pip-data-management-prod], Database [datamanagement]
 
-NOTICE:  DTSRD-xxxxx Step 1 : Rows INSERT by query=25
+NOTICE:  DTSRD-xxxxx Step 1 : Rows INSERT by query=13
+NOTICE:  DTSRD-xxxxx Step 2 : Rows DELETED by query=13
 NOTICE:  DTSRD-xxxxx SUCCESSFUL COMPLETION, all changes COMMITTED.
 */
