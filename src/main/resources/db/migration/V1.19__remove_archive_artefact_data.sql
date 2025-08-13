5e7353949f3e469bf026759b1ea29f19ddd62771
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-08-13 17:01:37 +0100

    make sure only archived artefact deleted

--- src/main/resources/db/migration/V1.19__remove_archive_artefact_data.sql
+++ src/main/resources/db/migration/V1.19__remove_archive_artefact_data.sql
@@ -5,8 +5,12 @@ DO
 $CR$
   DECLARE
   BEGIN
-    DELETE FROM artefact
-    WHERE is_archived = TRUE;
+    DELETE FROM artefact a
+      USING artefact_archived aa
+    WHERE a.artefact_id = aa.artefact_id
+      AND a.is_archived = TRUE;
+
+    RAISE NOTICE 'All archived artefacts have been deleted successfully.';
 
   EXCEPTION
     WHEN OTHERS THEN
