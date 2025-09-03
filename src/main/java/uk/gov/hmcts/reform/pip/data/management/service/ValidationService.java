2c78115cc3dc774ab0168e09e19d70aea4c6789b
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-09-03 11:48:35 +0100

    PUB-2993 Do not send PDDA file to AWS bucket on Production (#773)
    * Upload Html to AWS S3 Bucket
    
    * Start writing tests
    
    * Add tests
    
    * fix build
    
    * fix code conflicts
    
    * update pip data model
    
    * fix code review comments
    
    * fix code review comment
    
    * fixing build
    
    * update pip data model
    
    * fixed code review comment
    
    * fix build
    
    * Added functional test to upload html file
    
    * Fixed the PR comments
    
    * Do not send PDDA file to AWS bucket on Production
    
    * change pip data model version
    
    * add missing integration test
    
    * fix pip data model version
    
    * fix the response message
    
    * fix code review comments
    
    * add pip data model
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: ashwini.makanakattevenkatesha <ashwini.venkatesha@justice.gov.uk>

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/service/ValidationService.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/service/ValidationService.java
@@ -85,9 +85,20 @@ public class ValidationService {
         handleStringValidation(headers);
         handleDateValidation(headers);
         handleDefaultSensitivity(headers);
+        handleListValidation(headers);
         handleListTypeDeprecation(headers);
 
         return headers;
+    }
+
+    /**
+     * If publication type is LIST, it must have LIST_TYPE header.
+     * @param headers The headers to validate.
+     */
+    private void handleListValidation(HeaderGroup headers) {
+        if (headers.getType().equals(ArtefactType.LIST)) {
+            validateRequiredHeader(PublicationConfiguration.LIST_TYPE, headers.getListType());
+        }
     }
 
     /**
