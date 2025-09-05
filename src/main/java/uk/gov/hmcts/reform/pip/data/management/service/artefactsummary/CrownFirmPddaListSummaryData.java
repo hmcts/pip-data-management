ff5a5e7a7004319c115b0bedadf6b0b62ae49a73
natashaalker <natashaalker@yahoo.co.uk>
2025-09-05 16:30:04 +0100

    Add email summary and test

---
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/service/artefactsummary/CrownFirmPddaListSummaryData.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.pip.data.management.service.artefactsummary;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableMap;
+import uk.gov.hmcts.reform.pip.data.management.service.helpers.listmanipulation.CrownFirmPddaListHelper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class CrownFirmPddaListSummaryData implements ArtefactSummaryData {
+    @Override
+    public Map<String, List<Map<String, String>>> get(JsonNode payload) {
+        CrownFirmPddaListHelper.crownFirmPddaListFormatted(payload);
+        return processCrownPddaFirmList(payload);
+    }
+
+    private Map<String, List<Map<String, String>>> processCrownPddaFirmList(JsonNode node) {
+        List<Map<String, String>> summaryCases = new ArrayList<>();
+        CrownFirmPddaListHelper.crownFirmPddaListFormatted(node).forEach(item ->
+            item.getSittings().forEach(sitting ->
+                sitting.getHearings().forEach(hearing -> {
+                    Map<String, String> map = ImmutableMap.of(
+                        "Defendant", hearing.getDefendantName(),
+                        "Prosecuting authority", hearing.getProsecutingAuthority(),
+                        "Case reference", hearing.getCaseNumber(),
+                        "Hearing type", hearing.getHearingType()
+                    );
+                    summaryCases.add(map);
+                })
+            )
+        );
+        return Collections.singletonMap(null, summaryCases);
+    }
+}
