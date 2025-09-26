bfcb5b670391a0ca914121530ff611c361db25d0
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-09-26 16:49:49 +0100

    PUB-3009 - Updated functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/pip/data/management/publication/PublicationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pip/data/management/publication/PublicationTest.java
@@ -285,7 +285,7 @@ class PublicationTest extends FunctionalTestBase {
         assertThat(returnedGetArtefactMetadata.getArtefactId().toString()).isEqualTo(artefactId);
 
         Map<String, String> deleteArtefactHeaderMap = getBaseHeaderMap();
-        deleteArtefactHeaderMap.put(REQUESTER_ID_HEADER, EMAIL);
+        deleteArtefactHeaderMap.put(REQUESTER_ID_HEADER, systemAdminUserId);
 
         final Response responseGetAllRelevantArtefactsByLocationId = doGetRequest(
             ARTEFACT_BY_LOCATION_ID_URL + courtId, headerMap
@@ -335,7 +335,7 @@ class PublicationTest extends FunctionalTestBase {
         assertThat(returnedGetArtefactMetadata.getArtefactId().toString()).isEqualTo(artefactId);
 
         Map<String, String> deleteArtefactHeaderMap = getBaseHeaderMap();
-        deleteArtefactHeaderMap.put(REQUESTER_ID_HEADER, EMAIL);
+        deleteArtefactHeaderMap.put(REQUESTER_ID_HEADER, systemAdminUserId);
 
         final Response responseDeleteArtefact = doDeleteRequest(
             PUBLICATION_URL + '/' + artefactId, deleteArtefactHeaderMap
