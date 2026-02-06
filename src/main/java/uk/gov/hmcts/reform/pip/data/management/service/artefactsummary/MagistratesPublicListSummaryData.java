f33d2008a8ae6e9441fe125301ae021789909969
natashaalker <natashaalker@yahoo.co.uk>
2026-02-06 09:44:39 +0000

    Update email summary

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/service/artefactsummary/MagistratesPublicListSummaryData.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/service/artefactsummary/MagistratesPublicListSummaryData.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableMap;
 import uk.gov.hmcts.reform.pip.data.management.service.helpers.GeneralHelper;
 import uk.gov.hmcts.reform.pip.data.management.service.helpers.listmanipulation.MagistratesPublicListHelper;
-import uk.gov.hmcts.reform.pip.model.publication.Language;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -14,7 +13,7 @@ import java.util.Map;
 public class MagistratesPublicListSummaryData implements ArtefactSummaryData {
     @Override
     public Map<String, List<Map<String, String>>> get(JsonNode payload) {
-        MagistratesPublicListHelper.manipulatedMagistratesPublicListData(payload, Language.ENGLISH);
+        MagistratesPublicListHelper.manipulatedMagistratesPublicListData(payload);
         return processMagistratesPublicList(payload);
     }
 
@@ -24,21 +23,36 @@ public class MagistratesPublicListSummaryData implements ArtefactSummaryData {
             courtList -> courtList.get("courtHouse").get("courtRoom").forEach(
                 courtRoom -> courtRoom.get("session").forEach(
                     session -> session.get("sittings").forEach(
-                        sitting -> sitting.get("hearing").forEach(
-                            hearing -> hearing.get("case").forEach(hearingCase -> {
-                                Map<String, String> fields = ImmutableMap.of(
-                                    "Name",
-                                    GeneralHelper.findAndReturnNodeText(hearingCase, "defendant"),
-                                    "Prosecuting authority",
-                                    GeneralHelper.findAndReturnNodeText(hearingCase, "prosecutingAuthority"),
-                                    "URN",
-                                    GeneralHelper.findAndReturnNodeText(hearingCase, "caseUrn"),
-                                    "Hearing type",
-                                    GeneralHelper.findAndReturnNodeText(hearing, "hearingType")
-                                );
-                                summaryCases.add(fields);
-                            })
-                        )
+                        sitting -> sitting.get("hearing").forEach(hearing -> {
+                            if (hearing.has("case")) {
+                                hearing.get("case").forEach(hearingCase -> {
+                                    Map<String, String> fields = ImmutableMap.of(
+                                        "Name",
+                                        GeneralHelper.findAndReturnNodeText(hearingCase, "defendant"),
+                                        "Prosecuting authority",
+                                        GeneralHelper.findAndReturnNodeText(hearingCase, "prosecutingAuthority"),
+                                        "URN",
+                                        GeneralHelper.findAndReturnNodeText(hearingCase, "caseUrn"),
+                                        "Hearing type",
+                                        GeneralHelper.findAndReturnNodeText(hearing, "hearingType")
+                                    );
+                                    summaryCases.add(fields);
+                                });
+                            }
+                            if (hearing.has("application")) {
+                                hearing.get("application").forEach(application -> {
+                                    Map<String, String> fields = ImmutableMap.of(
+                                        "Name",
+                                        GeneralHelper.findAndReturnNodeText(application, "defendant"),
+                                        "Prosecuting authority",
+                                        GeneralHelper.findAndReturnNodeText(application, "prosecutingAuthority"),
+                                        "URN",
+                                        GeneralHelper.findAndReturnNodeText(application, "applicationReference")
+                                    );
+                                    summaryCases.add(fields);
+                                });
+                            }
+                        })
                     )
                 )
             )
