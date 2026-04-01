35b2f5d8f3f0849ce962a177ef4f7f223bfe87cb
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-04-01 13:30:35 +0100

    Revert "Recreate unique publication constraint on Artefact table (#865)"
    This reverts commit 7c42a497ffadac9f71e40d815a78c920293acfc4.

--- src/main/resources/db/migration/V1.22__recreate_unique_publication_index.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE artefact
-  DROP CONSTRAINT IF EXISTS unique_publication_index,
-  ADD CONSTRAINT unique_publication_index UNIQUE (location_id, content_date, language, list_type, provenance);
