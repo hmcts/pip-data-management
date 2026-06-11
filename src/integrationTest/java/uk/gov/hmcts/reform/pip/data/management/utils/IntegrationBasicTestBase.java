579ccefbe5ff518d7b3f3d896939efc2dbaf0bd0
KianKwa <kian.kwa@justice.gov.uk>
2026-06-11 10:46:50 +0100

    Add new mock bean for integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/data/management/utils/IntegrationBasicTestBase.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/data/management/utils/IntegrationBasicTestBase.java
@@ -3,8 +3,10 @@ package uk.gov.hmcts.reform.pip.data.management.utils;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import uk.gov.hmcts.reform.pip.data.management.database.ArtefactArchivedRepository;
 import uk.gov.hmcts.reform.pip.data.management.database.ArtefactRepository;
+import uk.gov.hmcts.reform.pip.data.management.database.ListSearchConfigRepository;
 import uk.gov.hmcts.reform.pip.data.management.database.LocationMetadataRepository;
 import uk.gov.hmcts.reform.pip.data.management.database.LocationRepository;
+import uk.gov.hmcts.reform.pip.data.management.models.publication.ListSearchConfig;
 
 public class IntegrationBasicTestBase extends IntegrationCommonTestBase {
     @MockitoBean
@@ -15,6 +17,9 @@ public class IntegrationBasicTestBase extends IntegrationCommonTestBase {
 
     @MockitoBean
     ArtefactArchivedRepository artefactArchivedRepository;
+
+    @MockitoBean
+    ListSearchConfigRepository listSearchConfigRepository;
 
     @MockitoBean
     protected LocationMetadataRepository locationMetadataRepository;
