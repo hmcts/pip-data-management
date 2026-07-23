640daf21ac86b852e2a89b98ee39782d11ab508b
KianKwa <kian.kwa@justice.gov.uk>
2026-07-23 14:02:42 +0100

    Fix sonarqube code duplication

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/service/filegeneration/MagistratesPublicAdultCourtListFileConverter.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/service/filegeneration/MagistratesPublicAdultCourtListFileConverter.java
@@ -24,18 +24,7 @@ public class MagistratesPublicAdultCourtListFileConverter extends ExcelAbstractL
         context.setVariable("contentDate", metadata.get("contentDate"));
         context.setVariable("locationName", metadata.get("locationName"));
 
-        JsonNode payloadDocument = payload.get("document");
-        String publicationDate = payloadDocument.get("data").get("job").get("printdate").asText();
-        context.setVariable("publicationDate",
-                            DateHelper.convertDateFormat(publicationDate, "dd/MM/yyyy"));
-
-        if (payloadDocument.has("info") && payloadDocument.get("info").has("start_time")) {
-            String publicationTime = payloadDocument.get("info").get("start_time").asText();
-            context.setVariable("publicationTime",
-                                DateHelper.convertTimeFormat(publicationTime, "HH:mm:ss"));
-        } else {
-            context.setVariable("publicationTime", "");
-        }
+        MagistratesAdultCourtListHelper.processDateInfo(context, payload);
 
         Language language = Language.valueOf(metadata.get("language"));
         languageResources.putAll(LanguageResourceHelper.readResourcesFromPath("common/linkToFact", language));
