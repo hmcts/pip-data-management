a4744ec583065c0ca9caabf1e6f26600b2f7c920
KianKwa <kian.kwa@justice.gov.uk>
2026-08-17 09:39:35 +0100

    fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/pip/data/management/service/publication/PublicationRemovalServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/data/management/service/publication/PublicationRemovalServiceTest.java
@@ -39,7 +39,6 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
