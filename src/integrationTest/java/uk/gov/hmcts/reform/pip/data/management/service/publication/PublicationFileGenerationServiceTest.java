349f26670bf64553ff384387c444840189d65aa2
abottenberg <alexbottenberg@hotmail.com>
2026-08-21 16:57:22 +0100

    Fixing testGenerateFilesNonSjpWelsh test

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/data/management/service/publication/PublicationFileGenerationServiceTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/data/management/service/publication/PublicationFileGenerationServiceTest.java
@@ -186,7 +186,7 @@ class PublicationFileGenerationServiceTest extends IntegrationBasicTestBase {
             .isEmpty();
 
         softly.assertThat(files.get().getExcel())
-            .as(FILE_NOT_EMPTY_MESSAGE)
+            .as(FILE_EMPTY_MESSAGE)
             .isNotEmpty();
 
         softly.assertAll();
@@ -211,11 +211,11 @@ class PublicationFileGenerationServiceTest extends IntegrationBasicTestBase {
             .isNotEmpty();
 
         softly.assertThat(files.get().getAdditionalPdf())
-            .as(FILE_NOT_EMPTY_MESSAGE)
-            .isNotEmpty();
+            .as(FILE_EMPTY_MESSAGE)
+            .isEmpty();
 
         softly.assertThat(files.get().getExcel())
-            .as(FILE_NOT_EMPTY_MESSAGE)
+            .as(FILE_EMPTY_MESSAGE)
             .isNotEmpty();
 
         softly.assertAll();
