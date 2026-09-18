9a159b0cf9206a757d59763b58acc8b1f3c70978
KianKwa <kian.kwa@justice.gov.uk>
2026-09-18 13:53:55 +0100

    Update web client config

--- src/main/java/uk/gov/hmcts/reform/pip/data/management/config/WebClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pip/data/management/config/WebClientConfiguration.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -44,12 +46,26 @@ public class WebClientConfiguration {
     @Bean
     @Profile("!dev")
     WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
-        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
-            new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        oauth2Client.setDefaultClientRegistrationId("accountManagementApi");
         return WebClient.builder()
             .exchangeStrategies(STRATEGIES)
-            .apply(oauth2Client.oauth2Configuration())
+            .filter((request, next) -> next.exchange(withBearerToken(request, authorizedClientManager)))
+            .build();
+    }
+
+    private static ClientRequest withBearerToken(ClientRequest request,
+                                                 OAuth2AuthorizedClientManager authorizedClientManager) {
+        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+            .withClientRegistrationId("accountManagementApi")
+            .principal("pip-data-management")
+            .build();
+        OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+
+        if (authorizedClient == null) {
+            return request;
+        }
+
+        return ClientRequest.from(request)
+            .headers(headers -> headers.setBearerAuth(authorizedClient.getAccessToken().getTokenValue()))
             .build();
     }
 
